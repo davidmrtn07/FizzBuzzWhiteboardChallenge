ee24771948a00134f08a734b994d2ff8a9f4d2a1
davidmrtn07 <davidmrtn07@gmail.com>
2024-05-19 15:46:23 -0500

    first Save

---
+++ repos/FizzBuzzWhiteboardChallenge/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+
+string FizzBuzz(int number)
+{
+    var answer = "";
+    if (number % 3 == 0)
+    {
+        answer = "fizz";
+    }
+    if(number % 5 == 0)
+    {
+        answer += "buzz";
+    }
+    return answer;
+}
